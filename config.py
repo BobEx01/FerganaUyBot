bd3a7b526d12c929824cd481169382886c0e436d
BobEx01 <boburi9000@mail.ru>
2025-07-28 22:48:58 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+API_TOKEN = "YOUR_BOT_TOKEN"
+CHANNELS = {
+    "kvartira": "@KvartiraFergana",
+    "uchastka": "@UchastkaFergana",
+    "yer": "@YerlarFergana"
+}
+LANGUAGES = ["uz", "uz_kiril", "ru", "en"]
