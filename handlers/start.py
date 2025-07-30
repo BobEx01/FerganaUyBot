9ae48c8c64c5da3557bf77fae9b9fa88158271c0
BobEx01 <boburi9000@mail.ru>
2025-07-30 19:00:32 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,2 +1,10 @@
-def start_handler():
-    print("Start handler activated. Til tanlash va salomlashish.")
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
+from keyboards.menu import main_menu
+from config import WELCOME_TEXT
+
+async def start_command(message: types.Message):
+    await message.answer(WELCOME_TEXT, reply_markup=main_menu())
+
+def register_start(dp: Dispatcher):
+    dp.register_message_handler(start_command, commands=["start"])
