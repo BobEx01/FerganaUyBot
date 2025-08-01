8ad242f4bf35cfe4f49e11e9873dbd9bc31a184f
BobEx01 <boburi9000@mail.ru>
2025-08-02 04:24:56 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -6,5 +6,5 @@ from config import WELCOME_TEXT
 async def start_command(message: types.Message):
     await message.answer(WELCOME_TEXT, reply_markup=main_menu())
 
-def register_start(dp: Dispatcher):
+def register_start_handlers(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=["start"])
